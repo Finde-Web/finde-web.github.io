ed312517c2cceb830b32633ddecf404dd2a914a6
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-08-26 10:58:32 -0400

    asd

--- finde-web/src/pages/Home.vue
+++ finde-web/src/pages/Home.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Página Inicial</h1>
+  <div class="background">
+    <video autoplay muted loop playsinline>
+      <source :src="bgVideo" type="video/mp4" />
+    </video>
+    <div class="content">
+      <h1 class="titulo">FINDE AWARDS 2025</h1>
+      <p class="sub-titulo">O mais aguardado voltou</p>
+      <button>Veja sobre</button>
+    </div>
+  </div>
+  <h2>a sdljkfalskdj</h2>
 </template>
+
+<script setup>
+import bgVideo from "../assets/BGNOISE.mp4";
+</script>
