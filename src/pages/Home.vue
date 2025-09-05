d98caf2fff739df9af6de3661e9cd01604ffcd6a
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-09-04 23:17:27 -0400

    hu

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="background">
-    <video autoplay muted loop playsinline>
+  <div class="background"> <video autoplay muted loop playsinline>
       <source :src="bgVideo" type="video/mp4" />
     </video>
     <div class="content">
-      <h1 class="titulo">FINDE AWARDS 2025</h1>
-      <p class="sub-titulo">O mais aguardado voltou</p>
-      <button>Veja sobre</button>
+      <h1 class="titulo-video">FINDE AWARDS 2025</h1>
+      <p class="sub-titulo">O mais aguardado voltou</p> <button>Veja sobre</button>
     </div>
   </div>
-  <h2>a sdljkfalskdj</h2>
+  <section>
+    <h2 class="titulo">Quem nós somos</h2>
+    <div class="duas-colunas">
+      <div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis ipsa voluptas asperiores aliquid possimus
+          tempora ducimus necessitatibus corrupti delectus exercitationem dolorum maxime quidem voluptatibus doloremque
+          eligendi, vitae obcaecati quia harum?</p>
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, saepe. Ad earum qui consectetur quae
+          dignissimos, possimus aperiam incidunt perferendis id nisi nulla sequi voluptatum placeat obcaecati excepturi
+          eligendi nostrum..</p>
+      </div>
+    </div>
+  </section>
 </template>
-
-<script setup>
-import bgVideo from "../assets/BGNOISE.mp4";
-</script>
+<script setup>import bgVideo from "../assets/BGNOISE.mp4"; </script>
